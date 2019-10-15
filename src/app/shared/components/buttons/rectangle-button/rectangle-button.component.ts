6b789869509b2d23adea3a21b635790bd4a3ff54
Kevin Cabrera Marrero <kevin.cabreramarrero@plexus.es>
2019-10-15 16:15:17 +0100

    Cambiado nombre del componente 'PrimaryButtonComponent' a 'RectangleButtonComponent' Añadido los cambios a todos los componentes en los que se utilizaba

---
+++ src/app/shared/components/buttons/rectangle-button/rectangle-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rectangle-button',
+  templateUrl: './rectangle-button.component.html',
+  styleUrls: ['./rectangle-button.component.less']
+})
+export class RectangleButtonComponent implements OnInit {
+
+  @Input() text: string;
+  @Input() style: string = 'primary';
+
+  ngOnInit(): void { }
+
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { AuxNavbarComponent } from './components/aux-navbar/aux-navbar.component';
 import { CircleButtonAuxComponent } from './components/buttons/circle-button-aux/circle-button-aux.component';
 import { CircleButtonComponent } from './components/buttons/circle-button/circle-button.component';
-import { PrimaryButtonComponent } from './components/buttons/primary-button/primary-button.component';
+import { RectangleButtonComponent } from './components/buttons/rectangle-button/rectangle-button.component';
 import { SecondaryButtonComponent } from './components/buttons/secondary-button/secondary-button.component';
 import { NavigationListCartComponent } from './components/navigation-list-cart/navigation-list-cart.component';
 import { PrimaryCardComponent } from './components/primary-card/primary-card.component';
@@ -23,7 +23,7 @@ import { InputFormComponent } from '../shared/components/form/input-form/input-f
     SearchBarComponent,
     SecondaryButtonComponent,
     PrimaryCardComponent,
-    PrimaryButtonComponent,
+    RectangleButtonComponent,
     AuxNavbarComponent,
     ToastComponent,
     NavigationListCartComponent,
@@ -40,7 +40,7 @@ import { InputFormComponent } from '../shared/components/form/input-form/input-f
     CircleButtonComponent,
     CircleButtonAuxComponent,
     NavigationListCartComponent,
-    PrimaryButtonComponent,
+    RectangleButtonComponent,
     SecondaryButtonComponent,
     AuxNavbarComponent,
     ToastComponent,
