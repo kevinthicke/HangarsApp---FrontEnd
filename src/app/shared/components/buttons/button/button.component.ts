9831daf07dfa2594b777943ad8028469295441b9
Kevin Cabrera Marrero <kevin.cabreramarrero@plexus.es>
2019-10-16 11:36:37 +0100

    Refactorizado el nombre del componente 'RectangleButton' por 'Button'

---
+++ src/app/shared/components/buttons/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.less']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: string;
+
+  ngOnInit(): void { }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,6 @@ import { InputFormComponent } from '../shared/components/form/input-form/input-f
 import { AuxNavbarComponent } from './components/aux-navbar/aux-navbar.component';
 import { CircleButtonAuxComponent } from './components/buttons/circle-button-aux/circle-button-aux.component';
 import { CircleButtonComponent } from './components/buttons/circle-button/circle-button.component';
-import { RectangleButtonComponent } from './components/buttons/rectangle-button/rectangle-button.component';
 import { SecondaryButtonComponent } from './components/buttons/secondary-button/secondary-button.component';
 import { NavigationListCartComponent } from './components/navigation-list-cart/navigation-list-cart.component';
 import { PrimaryCardComponent } from './components/primary-card/primary-card.component';
@@ -15,6 +14,7 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { ToastComponent } from './components/toast/toast.component';
 import { ButtonTypeDirective } from './directives/button-type.directive';
 import { HomeComponent } from './views/home/home.component';
+import { ButtonComponent } from './components/buttons/button/button.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { HomeComponent } from './views/home/home.component';
     SearchBarComponent,
     SecondaryButtonComponent,
     PrimaryCardComponent,
-    RectangleButtonComponent,
+    ButtonComponent,
     AuxNavbarComponent,
     ToastComponent,
     NavigationListCartComponent,
@@ -42,7 +42,7 @@ import { HomeComponent } from './views/home/home.component';
     CircleButtonComponent,
     CircleButtonAuxComponent,
     NavigationListCartComponent,
-    RectangleButtonComponent,
+    ButtonComponent,
     SecondaryButtonComponent,
     AuxNavbarComponent,
     ToastComponent,
